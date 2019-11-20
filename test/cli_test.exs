5ddcb38c55bf161e55e728c685bfbf62f69d0d69
Mirtles <mirte.post@yahoo.co.uk>
2019-11-20 11:03:10 +0100

    Add issue sort test

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -18,4 +18,16 @@ defmodule CliTest do
     assert parse_args(["user", "project"]) == { "user", "project", 4 }
   end
 
+  import Issues.CLI, only: [ parse_args: 1, sort_into_ascending_order: 1 ]
+
+  test "sort ascending orders the correct way" do
+    result = sort_into_ascending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{a b c}
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values, do: %{"created_at" => value, "other_data" => "xxx"}
+  end
+
 end
